2fe2065d8a2b7b6961ef04d52a52a06f5bc3367c
Tejas Bhosale <bhosaletejas93@gmail..com>
2018-12-16 22:30:18 +0530

    if the average pair exists in an array

---
+++ averagePair.js
@@ -0,0 +1,20 @@
+/* The problem is that given a sorted array and a average pair value. we want to find a pair that has that average pair value. */
+
+function averagePair(arr, val) {
+    let start = 0;
+    let end = arr.length - 1;
+    while (start < end) {
+        let avg = (arr[start] + arr[end]) / 2;
+        if (avg === val) {
+            return true;
+        } else if (avg < val) {
+            start++;
+        } else {
+            end--;
+        }
+
+    }
+    return false;
+}
+
+console.log(averagePair([1, 2, 3], 2.5));
